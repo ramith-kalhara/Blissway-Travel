c4a14e2f8b7da637b23256784183ba4b0b3debd7
Ramith Kalhara <kalhara7ramith17@gmail.com>
2025-01-20 13:09:29 +0530

    backend Spring

---
+++ BackEnd/blissway/blissway/src/main/java/com/blissway/BlisswayApplication.java
@@ -0,0 +1,13 @@
+package com.blissway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlisswayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlisswayApplication.class, args);
+	}
+
+}
---
+++ BackEnd/blissway/blissway/src/test/java/com/blissway/BlisswayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blissway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlisswayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
